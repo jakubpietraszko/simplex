3ff56e22b3e0bcff17d3b977636331de2c967ec1
jakub <jakub.p.15022003@gmail.com>
2023-08-01 15:39:49 +0200

    empty definitions in solver

--- src/solver/solver.cpp
+++ src/solver/solver.cpp
@@ -1,1 +1,33 @@
 #include "solver.hpp"
+
+//Solver
+
+void Solver::maximize(const Expression&expr)
+{
+
+}
+void Solver::minimize(const Expression&expr)
+{
+
+}
+void Solver::add_variable(const Var&var)
+{
+
+}
+void Solver::add_constrait(const Constrait&constrait)
+{
+
+}
+void Solver::solve()
+{
+
+}
+std::string Solver::show_result()
+{
+
+}
+
+Solver::operator std::string()
+{
+    
+}
