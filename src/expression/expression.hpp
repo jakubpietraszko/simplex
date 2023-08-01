6c62451b239d70c4daec3ae257ea924aec77d06b
jakub <jakub.p.15022003@gmail.com>
2023-08-01 14:34:01 +0200

    add declaration to expression_hpp

--- src/expression/expression.hpp
+++ src/expression/expression.hpp
@@ -1,12 +1,31 @@
 #pragma once
 #include "../data/data.hpp"
-
+class Monomial;
+class Expression;
 class Var
 {
 public:
     Var(std::string name)
         :name_{name},index_{count_}
         {++count_;}
+    
+    Monomial operator*(const MultType&m)const;
+    Monomial operator/(const MultType&m)const;
+
+    friend Monomial operator*(const MultType&m,const Var&var);
+
+    Expression operator+(const Monomial&mon)const;
+    Expression operator+(const Var&var)const;
+
+    Expression operator-(const Monomial&mon)const;
+    Expression operator-(const Var&var)const;
+
+    Var operator-()const;
+    Var operator+()const;
+
+    operator Monomial()const;
+    operator std::string()const;
+
 private:
     inline static IndexType count_=0;
     IndexType index_;
@@ -18,6 +37,26 @@ class Monomial
 public:
     Monomial(MultType mult,Var var)
         :mult_{mult},var_{var}{}
+
+    Monomial operator*(const MultType&m)const;
+    Monomial operator/(const MultType&m)const;
+
+    friend Monomial operator*(const MultType&m,const Monomial&mon);
+
+    Monomial& operator*=(const MultType&m);
+    Monomial& operator/=(const MultType&m);
+
+    Expression operator+(const Monomial&mon)const;
+    Expression operator+(const Var&var)const;
+
+    Expression operator-(const Monomial&mon)const;
+    Expression operator-(const Var&var)const;
+
+    Monomial operator-()const;
+    Monomial operator+()const;
+
+    operator std::string()const;
+
 private:
     MultType mult_;
     Var var_;
@@ -27,6 +66,31 @@ class Expression
 {
 public:
     Expression()=default;
+    Expression(const Monomial&mon);
+
+    Expression operator+(const Var&var)const;
+    Expression operator+(const Monomial&mon)const;
+    Expression operator-(const Var&var)const;
+    Expression operator-(const Monomial&mon)const;
+
+    Expression& operator+=(const Var&var);
+    Expression& operator+=(const Monomial&mon);
+    Expression& operator-=(const Var&var);
+    Expression& operator-=(const Monomial&mon);
+
+    Expression operator*(const MultType&m)const;
+    Expression& operator*=(const MultType&m);
+
+    Expression operator/(const MultType&m)const;
+    Expression& operator/=(const MultType&m);
+
+    Expression operator+()const;
+    Expression operator-()const;
+
+    void simplify();
+
+    operator std::string()const;
+
 private:
     std::vector<Var>polynomial_;
 };
