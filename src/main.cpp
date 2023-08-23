e4ad42f97b6824bc0df393b52390ec946cacd660
jakub <jakub.p.15022003@gmail.com>
2023-08-23 23:26:19 +0200

    make main

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,17 @@ int main()
 
     auto x1=Var{"x",1};
     auto x2=Var{"x",2};
+
     solver.add_variable(x1);
     solver.add_variable(x2);
+
     solver.minimize(3*x1-2*x2);
+    
     solver.add_constrait(x1+x2<=10);
     solver.add_constrait(x1 -x2<=-1);
     solver.add_constrait(2*x1 -x2<=-2);
-    //-20
+
     solver.solve();
-    solver.show_results();
+    solver.show_debug();
+
 }
